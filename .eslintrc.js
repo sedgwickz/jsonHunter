6c8d93142d052905bd80d9840fe0168d8b62ed6c
sedgwickz <kunnsh@gmail.com>
2020-12-18 19:00:17 +0800

    add eslint prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    extends: [
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'prettier',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+        node: true,
+        browser: true,
+        es6: true,
+        jest: true
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        'linebreak-style': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto'
+            }
+        ]
+    }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true
+}
